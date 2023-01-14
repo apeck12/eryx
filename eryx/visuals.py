02b261a4732c97fd5dd2af709b72a3d67990df47
apeck <apeck@slac.stanford.edu>
2023-01-13 21:57:32 -0800

    ignore nans

--- eryx/visuals.py
+++ eryx/visuals.py
@@ -15,10 +15,11 @@ def visualize_central_slices(I, vmax_scale=5):
     """
     f, (ax1,ax2,ax3) = plt.subplots(1, 3, figsize=(12,4))
     map_shape = I.shape
+    vmax = I[~np.isnan(I)].mean()*vmax_scale
     
-    ax1.imshow(I[int(map_shape[0]/2),:,:], vmax=I.mean()*vmax_scale)
-    ax2.imshow(I[:,int(map_shape[1]/2),:], vmax=I.mean()*vmax_scale)
-    ax3.imshow(I[:,:,int(map_shape[2]/2)], vmax=I.mean()*vmax_scale)
+    ax1.imshow(I[int(map_shape[0]/2),:,:], vmax=vmax)
+    ax2.imshow(I[:,int(map_shape[1]/2),:], vmax=vmax)
+    ax3.imshow(I[:,:,int(map_shape[2]/2)], vmax=vmax)
 
     ax1.set_aspect(map_shape[2]/map_shape[1])
     ax2.set_aspect(map_shape[2]/map_shape[0])
