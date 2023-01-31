89eaa6a31b96429695239bb7575d8e0cae2a55a6
fredericpoitevin <frederic.poitevin@gmail.com>
2023-01-31 08:40:41 -0800

    changed plot titles when visualizing slices as contour

--- eryx/visuals.py
+++ eryx/visuals.py
@@ -24,18 +24,24 @@ def visualize_central_slices(I, vmax_scale=5, contour=False):
         ax1.contourf(I[int(map_shape[0] / 2), :, :], origin='upper')
         ax2.contourf(I[:, int(map_shape[1] / 2), :], origin='upper')
         ax3.contourf(I[:, :, int(map_shape[2] / 2)], origin='upper')
+
+        ax1.set_title("View along h", fontsize=14)
+        ax2.set_title("View along k", fontsize=14)
+        ax3.set_title("View along l", fontsize=14)
+
     else:
         ax1.imshow(I[int(map_shape[0]/2),:,:], vmax=vmax)
         ax2.imshow(I[:,int(map_shape[1]/2),:], vmax=vmax)
         ax3.imshow(I[:,:,int(map_shape[2]/2)], vmax=vmax)
 
+        ax1.set_title("(0,k,l)", fontsize=14)
+        ax2.set_title("(h,0,l)", fontsize=14)
+        ax3.set_title("(h,k,0)", fontsize=14)
+
     ax1.set_aspect(map_shape[2]/map_shape[1])
     ax2.set_aspect(map_shape[2]/map_shape[0])
     ax3.set_aspect(map_shape[1]/map_shape[0])
 
-    ax1.set_title("(0,k,l)", fontsize=14)
-    ax2.set_title("(h,0,l)", fontsize=14)
-    ax3.set_title("(h,k,0)", fontsize=14)
 
     for ax in [ax1,ax2,ax3]:
         ax.set_xticks([])
