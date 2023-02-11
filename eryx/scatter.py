6d610ba1484e70805aaee7e4b3eb32b99b08ca67
apeck <apeck@slac.stanford.edu>
2023-02-10 21:34:42 -0800

    changing parallelization to multiprocess, ray, or none

--- eryx/scatter.py
+++ eryx/scatter.py
@@ -1,6 +1,10 @@
 import numpy as np
+import ray
+import os
+import psutil
 import multiprocess as mp
 from functools import partial
+from tqdm import tqdm
 
 def compute_form_factors(q_grid, ff_a, ff_b, ff_c):
     """
@@ -81,22 +85,23 @@ def structure_factors_batch(q_grid, xyz, ff_a, ff_b, ff_c, U=None,
     return A 
 
 def structure_factors(q_grid, xyz, ff_a, ff_b, ff_c, U=None,
-                      batch_size=100000, n_processes=8,
+                      batch_size=100000, parallelize='multiprocess',
                       compute_qF=False, project_on_components=None,
                       sum_over_atoms=True):
     """
     Batched version of the structure factor calculation. See 
     docstring for structure_factors_batch for parameters and 
-    returns, with the exception of n_processes, which refers 
-    to the number of processors available. If greater than 1,
-    multiprocessing will be used.
+    returns, with the exception of the parallelize argument,
+    which can be None (for 1 core), 'multiprocess', or 'ray'.
+    We use psutil to automatically deduce the number of cores
+    available.
     """
     n_batches = q_grid.shape[0] // batch_size
     if n_batches == 0:
         n_batches = 1
     splits = np.append(np.arange(n_batches) * batch_size, np.array([q_grid.shape[0]]))
 
-    if n_processes == 1:
+    if parallelize is None:
         dim1_size = q_grid.shape[0]
         if sum_over_atoms:
             A_shape = dim1_size
@@ -112,11 +117,27 @@ def structure_factors(q_grid, xyz, ff_a, ff_b, ff_c, U=None,
             A[splits[batch]: splits[batch+1]] = structure_factors_batch(q_sel, xyz, ff_a, ff_b, ff_c, U=U, compute_qF=compute_qF,
                                                                         project_on_components=project_on_components, sum_over_atoms=sum_over_atoms)
     else:
+        num_cpus = psutil.cpu_count(logical=False)
         q_sel = [q_grid[splits[batch]: splits[batch+1]] for batch in range(n_batches)]
-        pool = mp.Pool(processes=n_processes)
-        sf_partial = partial(structure_factors_batch, xyz=xyz, ff_a=ff_a, ff_b=ff_b, ff_c=ff_c, U=U,
-                             compute_qF=compute_qF,
-                             project_on_components=project_on_components, sum_over_atoms=sum_over_atoms)
-        A = np.concatenate(pool.map(sf_partial, q_sel), axis=0)
+
+        if parallelize == 'multiprocess':
+            pool = mp.Pool(processes=num_cpus)
+            sf_partial = partial(structure_factors_batch, xyz=xyz, ff_a=ff_a, ff_b=ff_b, ff_c=ff_c, U=U,
+                                 compute_qF=compute_qF, project_on_components=project_on_components, sum_over_atoms=sum_over_atoms)
+            A = np.concatenate(list(tqdm(pool.imap(sf_partial, q_sel), total=len(q_sel))), axis=0)
+
+        elif parallelize == 'ray':
+            os.environ['PYTHONPATH'] = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            ray.init()
+            A = ray.get([partial(ray.remote(structure_factors_batch).remote,
+                                 xyz=xyz, ff_a=ff_a, ff_b=ff_b, ff_c=ff_c, U=U,
+                                 compute_qF=compute_qF,
+                                 project_on_components=project_on_components,
+                                 sum_over_atoms=sum_over_atoms)(q_grid=q_sel[i]) for i in range(len(q_sel))])
+            A = np.concatenate(A, axis=0)
+            ray.shutdown()
+
+        else:
+            raise ValueError("parallize argument not recognized; must be None, multiprocess, or ray")
        
     return A
