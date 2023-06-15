c9b93e5da98f02774dcaf586b8a689b0ec26dc75
apeck <apeck@slac.stanford.edu>
2023-06-15 10:48:30 -0700

    actually use said b factors

--- eryx/models.py
+++ eryx/models.py
@@ -498,13 +498,14 @@ class RigidBodyRotations:
                 # apply Guinier's equation to rotated ensemble
                 fc = np.zeros(self.q_grid.shape[0], dtype=complex)
                 fc_square = np.zeros(self.q_grid.shape[0])
+                U = self.model.adp[asu] / (8 * np.pi * np.pi)
                 for rnum in range(num_rot):
                     A = structure_factors(self.q_grid[self.mask], 
                                           xyz_rot[rnum], 
                                           self.model.ff_a[asu], 
                                           self.model.ff_b[asu], 
                                           self.model.ff_c[asu], 
-                                          U=None, 
+                                          U=U, 
                                           batch_size=self.batch_size,
                                           n_processes=self.n_processes)
                     if ensemble_dir is not None:
@@ -637,7 +638,7 @@ class Ensemble:
                                       self.model.ff_a[index], 
                                       self.model.ff_b[index], 
                                       self.model.ff_c[index], 
-                                      U=None, 
+                                      U=U, 
                                       batch_size=self.batch_size,
                                       n_processes=self.n_processes)
 
